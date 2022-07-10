f23b4af116704b0bd807dcdf04386e2c9654e2c5
Daniel Silva <103064271+Daniel-Developement@users.noreply.github.com>
2022-07-10 00:35:58 -0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 const body = document.querySelector('body');
 const prefersColorScheme = window.matchMedia('(prefers-color-scheme: dark)');
 
-if (prefersColorScheme.matches) {
-    body.classList.add('dark');
-} else {
-    body.classList.remove('dark');
-}
+setInterval(() => {
+    if (prefersColorScheme.matches) {
+        body.classList.add('dark');
+    } else {
+        body.classList.remove('dark');
+    }
+}, 1000);
